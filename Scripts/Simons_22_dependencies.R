3eee78f58d28090c0cb9dccf4055906457e7e971
Dina-Leigh Simons <dina.leigh.simons@gmail.com>
2022-07-28 12:52:26 +0100

    added new script for packages

---
+++ Scripts/Simons_22_dependencies.R
@@ -0,0 +1,18 @@
+#Simons, Bradbury and Evans (2022) main script dependencies
+
+#Setting environment----
+rm(list = ls())
+
+#Install required packages----
+install.packages("ggpubr")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("car")
+install.packages("survey")
+install.packages("Hmisc")
+install.packages("corrplot")
+install.packages("ggfortify")
+install.packages("jtools")
+install.packages("ggstance")
+install.packages("broom.mixed")
+install.packages("plotrix")
