77d04457238c906980f93c9a4b2a3ee8a13df1dd
Dina-Leigh Simons <dina.leigh.simons@gmail.com>
2024-01-04 14:32:07 +0000

    update power analyses

--- Scripts/Simons_23_power.R
+++ Scripts/Simons_23_power.R
@@ -32,6 +32,28 @@ pwr.f2.test(u = 19,
 
 #with above parameters, model is 85.6% powered
 
+#general linear model 2----
+#(https://data-se.netlify.app/2018/07/24/power-calculation-for-the-general-linear-model/)
+
+pwr.f2.test(u = 17,
+            v = NULL,
+            f2 = 0.02,
+            sig.level = 0.05, 
+            power = 0.8)
+
+982 + 17
+
+#u = degrees of freedom for numerator
+#v = degrees of freedom for denominator
+#f2 =  	effect size (R2/(1-R2) where R2 is proportion of variance accounted)
+#sig.level = Significance level (Type I error probability)
+#power = Power of test (1 minus Type II error probability)
+
+#v = 982
+#n = v + p = 982 + 17
+#n = 999 overall, 167 per treatment (80% power, 17 variables, sig level = 0.05)
+
+
 #for interaction, general advice is to reduce effect size to account for this
 pwr.f2.test(u = 19,
             v = NULL,
